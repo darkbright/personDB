ccb507690f6a529ec7a669fdcc912e0d0cc35fa4
leezy <leeg1dance@gmail.com>
2022-08-31 15:38:55 +0900

    ItemSide key changed

--- em/components/layout.tsx
+++ em/components/layout.tsx
@@ -1,17 +1,10 @@
-/**
- * https://ant.design/components/layout/
- * Header Sider 2 참고
- */
-
- import { LaptopOutlined, SettingTwoTone, ToolTwoTone,NotificationOutlined, UserOutlined } from '@ant-design/icons';
- import { Breadcrumb, Layout, Menu } from 'antd';
+ import { ToolOutlined, UserSwitchOutlined, AuditOutlined, AlertOutlined, SettingOutlined } from '@ant-design/icons';
+ import { Breadcrumb, Layout, Menu, Typography} from 'antd';
  import React from 'react';
+
  const { Header, Content, Sider } = Layout;
- const items1 = ['1', '2', '3'].map((key) => ({
-   key,
-   label: `nav ${key}`,
- }));
- const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined,ToolTwoTone,SettingTwoTone].map((icon, index) => {
+ const { Text } = Typography;
+ const items2 = [ AuditOutlined, UserSwitchOutlined, ToolOutlined, SettingOutlined ].map((icon, index) => {
    const key = String(index + 1);
    return {
      key: `sub${key}`,
@@ -26,22 +19,20 @@
      }),
    };
  });
- 
  const itemsSide = [
    {
      key: "sub1",
-     icon: React.createElement(UserOutlined),
+     icon: React.createElement(ToolOutlined),
      label: '장비관리',
      children: [
        {key: '01', label: '장비관리'},
        {key: '02', label: '단말기관리'},
        {key: '03', label: 'QR코드관리'},
-    //    {key: '04', label: '논란중인'},
      ]
    },
    {
      key: "sub2",
-     icon: React.createElement(LaptopOutlined),
+     icon: React.createElement(UserSwitchOutlined),
      label: '사용자관리',
      children: [
        {key: '11', label: '관리자'},
@@ -51,27 +42,27 @@
    },
    {
     key: "sub3",
-    icon: React.createElement(UserOutlined),
-    label: '기관/제작업체관리',
+    icon: React.createElement(AuditOutlined),
+    label: '기관 / 제작업체관리',
     children: [
-      {key: '01', label: '기관'},
-      {key: '02', label: '제작업체관리'},
+      {key: '21', label: '기관'},
+      {key: '22', label: '제작업체관리'},
     ]
   },
   {
     key: "sub4",
-    icon: React.createElement(ToolTwoTone),
+    icon: React.createElement(AlertOutlined),
     label: '사고관리',
     children: [
-      {key: '01', label: '사고관리'},
+      {key: '31', label: '사고관리'},
     ]
   },
   {
     key: "sub5",
-    icon: React.createElement(SettingTwoTone),
+    icon: React.createElement(SettingOutlined),
     label: '시스템관리',
     children: [
-      {key: '01', label: '시스템관리'},
+      {key: '41', label: '시스템관리'},
     ]
   },
  ]
@@ -79,8 +70,9 @@
  const App = ( {children} ) => (
    <Layout>
      <Header className="header">
-       <div className="logo" />
-       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+       {/* <div className="logo" /> */}
+       <Text strong style={ {fontSize: '20px', color: 'white'} }> OO장비 관리 시스템</Text>
+       {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} /> */}
      </Header>
      <Layout>
        <Sider width={200} className="site-layout-background">
@@ -93,7 +85,7 @@
              borderRight: 0,
            }}
            items={itemsSide}
-         />
+        />
        </Sider>
        <Layout
          style={{
@@ -105,9 +97,8 @@
              margin: '16px 0',
            }}
          >
-           <Breadcrumb.Item>Home</Breadcrumb.Item>
-           <Breadcrumb.Item>List</Breadcrumb.Item>
-           <Breadcrumb.Item>App</Breadcrumb.Item>
+           <Breadcrumb.Item href="..">장비관리</Breadcrumb.Item>
+           <Breadcrumb.Item href="./table">Table</Breadcrumb.Item>
          </Breadcrumb>
          <Content
            className="site-layout-background"
