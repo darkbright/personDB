c332a03c4d36995c021697dc4cfe45d22bd8fa9f
LEEZY <leezy97html@gmail.com>
2022-09-14 16:40:46 +0900

    Construction modified (menu, icon,key,label)

--- em/components/layout.tsx
+++ em/components/layout.tsx
@@ -1,4 +1,4 @@
- import { ToolOutlined, UserSwitchOutlined, AuditOutlined, AlertOutlined, SettingOutlined } from '@ant-design/icons';
+ import { ToolOutlined,CloudServerOutlined, UserSwitchOutlined,ControlOutlined, AuditOutlined, ApartmentOutlined,ThunderboltOutlined,SettingOutlined } from '@ant-design/icons';
  import { Breadcrumb, Layout, Menu, Typography} from 'antd';
  import React from 'react';
  import Router from 'next/router';
@@ -20,89 +20,73 @@
   },
   {
      key: "equipment",
-     icon: React.createElement(ToolOutlined),
-     label: '장비관리',
+     icon: React.createElement(CloudServerOutlined),
+     label: '암호장비 관리',
      children: [
-       {key: 'info',        label: 'OO장비 정보'},
-       {key: 'user',        label: '사용자 승인정보'},
-       {key: 'produce',     label: '제작 승인정보'},
-       {key: 'status',      label: 'OO장비 상태정보'},
-       {key: 'possession',  label: '보유현황 정보'},
-       {key: 'tracking',    label: 'OO장비 추적'},
-       {key: 'certificate', label: '증명서 정보'},
-       {key: 'management',  label: '전자 관리기록부'},
-       {key: 'proof',       label: '전자증명서'},
-       {key: 'approval',    label: '승인업무 처리'},
-     ]
-   },
+       {key: 'info',        label: '장비 정보'},
+       {key: 'status',      label: '상태 정보'},
+       {key: 'tracking',    label: '장비 추적'},
+       {key: 'possession',  label: '보유 현황'},
+       {key: 'user',        label: '수발 관리'},
+       {key: 'produce',     label: '장비 정비 이력'},
+       {key: 'certificate', label: '보안사고정보관리'},
+    ]
+  },
    {
     key: "terminal",
-    icon: React.createElement(AuditOutlined),
-    label: '단말기 관리',
+    icon: React.createElement(ControlOutlined),
+    label: '암호자재 관리',
     children: [
-      {key: 'info',         label: '단말기 정보'},
-      {key: 'distribution', label: '단말기 배부'},
+      {key: 'info',         label: '증명서 관리'},
+      {key: 'distribution', label: '관리 기록부'},
     ]
   },
   {
     key: "qrcode",
     icon: React.createElement(AuditOutlined),
-    label: 'QR코드 관리',
+    label: '승인 관리',
     children: [
-      {key: 'info',     label: 'QR코드 정보'},
-      {key: 'approval', label: 'QR코드 승인'},
-      {key: 'approval', label: 'QR코드 인쇄'},
+      {key: 'info',     label: '사용 승인'},
+      {key: 'approval', label: '제작 승인'},
+      {key: 'approval', label: '파기 승인'},
     ]
   },
   {
     key: "vendor",
-    icon: React.createElement(AuditOutlined),
-    label: '제작업체 관리',
+    icon: React.createElement(ThunderboltOutlined),
+    label: '개발 관리',
     children: [
-      {key: 'appoint',  label: '제작업체 지정'},
-      {key: 'status',   label: '제작업체 현황'},
-      {key: 'analysis', label: '제작업체 분석'},
-      {key: 'security', label: '제작업체 보안측정'},
+      {key: 'appoint',  label: '개발 과제'},
+      {key: 'status',   label: '이슈 관리'},
+      {key: 'analysis', label: '시험정보 관리'},
+      {key: 'security', label: '평가정보 관리'},
     ]
   },
   {
     key: "develop",
     icon: React.createElement(ToolOutlined),
-    label: 'OO장비 개발정보 관리',
+    label: '제작업체 관리',
     children: [
-      {key: 'need',         label: '개발과제 소요관리'},
-      {key: 'issue',        label: '개발과제 이슈관리'},
-      {key: 'request',      label: '개발과제 요구사항 관리'},
-      {key: 'charge',       label: '개발담당 관리'},
-      {key: 'test',         label: '시험정보 관리'},
-      {key: 'evaluation',   label: '평가정보 관리'},
-      {key: 'verification', label: '검정정보 관리'},
+      {key: 'need',         label: '제작업체 관리'},
     ]
   },  
   {
     key: "organiztion",
-    icon: React.createElement(AuditOutlined),
+    icon: React.createElement(ApartmentOutlined),
     label: '유관기관 관리',
     children: [
-      {key: 'status',   label: '유관기관 현황'},
-      {key: 'council',  label: '협의회 관리'},
+      {key: 'status',   label: '유관기관 관리'},
+
     ]
   },  
   {
     key: "accident",
-    icon: React.createElement(AlertOutlined),
-    label: '사고관리',
-    children: [
-      {key: 'info', label: '사고정보 관리'},
-    ]
-  },
-  {
-    key: "system",
     icon: React.createElement(SettingOutlined),
-    label: '시스템관리',
+    label: '시스템 관리',
     children: [
-      {key: 'upload',   label: '데이터 업로드'},
-      {key: 'download', label: '데이터 다운로드'},
+      {key: 'info', label: '사용자 관리'},
+      {key: 'info', label: '단말기 관리'},
+      {key: 'info', label: '데이터 관리'},
     ]
   },
  ]
