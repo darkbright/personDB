4d080df21d447cdd52403231e09fd71ab736107c
LEEZY <leezy97html@gmail.com>
2022-09-15 10:35:29 +0900

    Add 3depth menu

--- em/components/layout.tsx
+++ em/components/layout.tsx
@@ -14,7 +14,10 @@
      icon: React.createElement(CloudServerOutlined),
      label: '암호장비 관리',
      children: [
-       {key: 'equipment',     label: '장비 정보'},
+       {  key: 'equipmentInfo',     
+          label: '장비 정보',
+          children:[{key: 'equipmentInfo', label: '장비/제작정보'}]
+        },
        {key: 'status',        label: '상태 정보'},
        {key: 'tracking',          label: '장비 추적'},
        {key: 'holding',     label: '보유 현황'},
@@ -48,11 +51,24 @@
     icon: React.createElement(ThunderboltOutlined),
     label: '개발 관리',
     children: [
-      {key: 'development',  label: '개발 과제'},
-      {key: 'issues',       label: '이슈 관리'},
-      {key: 'test',         label: '시험정보 관리'},
-      {key: 'rating',       label: '평가정보 관리'},
-      {key: 'verify',       label: '안정성 검증 관리'},
+      { key: 'assignment',  
+        label: '개발 과제',
+        children:[
+          {key: 'duration',    label: '개발과제/소요이력정보'},
+          {key: 'requirement', label: '요구사항관리'},
+        ]
+      },
+      {key: 'issues',  label: '이슈 관리'},
+      {
+        key: 'test',         
+        label: '시험정보 관리',
+        children:[
+          {key: 'general',   label: '일반 시험'},
+          {key: 'workplace', label: '현장 시험'},
+        ]
+      },
+      {key: 'rating',  label: '평가정보 관리'},
+      {key: 'verify',  label: '안정성 검증 관리'},
       {key: 'charge',  label: '개발담당 관리'},
     ]
   },
@@ -60,27 +76,41 @@
     key: "production",
     icon: React.createElement(ToolOutlined),
     label: '제작업체 관리',
-    children: [
-      {key: 'production',   label: '제작업체 관리'},
-    ]
+    children: [{key: 'production',   label: '제작업체 관리'},]
   },  
   {
     key: "organizations",
     icon: React.createElement(ApartmentOutlined),
     label: '유관기관 관리',
-    children: [
-      {key: 'organizations', label: '유관기관 관리'},
-
-    ]
+    children: [{key: 'organizations', label: '유관기관 관리'},]
   },  
   {
     key: "accident",
     icon: React.createElement(SettingOutlined),
     label: '시스템 관리',
     children: [
-      {key: 'system',   label: '사용자 관리'},
-      {key: 'terminal', label: '단말기 관리'},
-      {key: 'data',     label: '데이터 관리'},
+      {
+        key: 'system',   
+        label: '사용자 관리',
+        children:[
+          {key: 'manager', label: '관리자 관리'},
+          {key: 'user', label: '사용자 관리'},
+        ]
+      },
+      {
+        key: 'terminal', 
+        label: '단말기 관리',
+        children:[
+          {key: 'qr', label: 'QR코드 정보'},
+          {key: 'workplace', label: '단말기 관리'},
+        ]
+      },
+        
+      {
+        key: 'data',     
+        label: '데이터 관리',
+        children:[{key: 'upload', label: '데이터업로드/다운로드'},]
+      },
     ]
   },
  ]
